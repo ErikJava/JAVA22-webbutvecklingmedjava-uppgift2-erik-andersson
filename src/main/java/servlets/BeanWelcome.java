80d713245ff1e3ff1a4bde8b6334a3061f60a65b
ErikJava <112395602+ErikJava@users.noreply.github.com>
2022-12-09 00:28:36 +0100

    Uploaded all files

---
+++ src/main/java/servlets/BeanWelcome.java
@@ -0,0 +1,18 @@
+package servlets;
+
+public class BeanWelcome {	
+	
+	private String Name;
+	
+	public BeanWelcome() {		
+	}
+	
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String Name) {
+		this.Name = Name;
+	}	
+}
+
